f34cd49d684b5eedb615c1a7eadf7344285ec563
jodi <128635337+jodi-j@users.noreply.github.com>
2024-11-24 23:40:31 -0500

    Basic NavBar Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/home/Home';
+import Calculator from './pages/calculator/Calculator';
+import Recommendations from './pages/recommendations/Recommendations';
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/calculator' element={<Calculator/>}></Route>
+          <Route path='/recommendations' element={<Recommendations/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+      </Router>
   );
 }
 
---
+++ frontend/src/pages/recommendations/Recommendations.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Recommendations = () => {
+    return (
+        <h1>Recommendations Page</h1>
+    );
+};
+
+export default Recommendations;
