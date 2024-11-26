e03e0755e8b65cb776f6590756972e07badcfef7
mh4rs <mharissa@umich.edu>
2024-11-26 13:15:07 -0500

    Updated login page to match Figma design.

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import "./Login.css";
+import lockIcon from './lock.png';
+import personIcon from './person.png';
 
 const Login = () => {
-    return (
-        <h1>Login Page</h1>
-    );
+  return (
+    <div className="login-container">
+      <h1 className="title">CarbonWise</h1>
+      <div className="login-box">
+        <h2 className="welcome-text">Welcome!</h2>
+        <p className="subtext">Please enter your login credentials.</p>
+        <form>
+          <div className="input-group">
+            <label htmlFor="username">
+              <i className="username-icon"></i>
+            </label>
+            <img src={personIcon} alt="Username Icon" className="input-icon" />
+            <input type="text" id="username" className="username-input" placeholder="Username" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">
+              <i className="password-icon"></i>
+            </label>
+            <img src={lockIcon} alt="Password Icon" className="input-icon" />
+            <input type="password" id="password" className="password-input" placeholder="Password" />
+          </div>
+          <button type="submit" className="sign-in-btn">Sign In</button>
+        </form>
+        <div className="divider">or</div>
+        <button className="create-account-btn">Create an Account</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
