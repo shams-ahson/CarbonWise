362b156cb04b4cbada4e6ddfeb9049a6c0cfd921
jodi <128635337+jodi-j@users.noreply.github.com>
2024-12-04 23:06:26 -0500

    Display Carbon Footprint Score on Recommendations Page

--- frontend/src/pages/recommendations/Recommendations.js
+++ frontend/src/pages/recommendations/Recommendations.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Button from '../../components/Button';
 
 const Recommendations = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { totalEmissions } = location.state || {}; // Retrieve the state
+
     return (
-        <h1>Recommendations Page</h1>
+        <div className="centered-container">
+            <h1>Carbon Footprint Results</h1>
+            {totalEmissions !== undefined ? (
+                <div className="centered-container">
+                    <p>Your Total Carbon Footprint: {totalEmissions.toFixed(2)} CO2 tons/year</p>
+
+                    <h1>Sustainability Recommendations</h1>
+                    <p>Here are some recommendations to offset your carbon footprint based on your results.</p>
+                </div>
+            ) : (
+                <div>
+                    <p>Please complete the calculator quiz in order to understand your carbon footprint!</p>
+                    
+                    <Button
+                        label="Take Carbon Footprint Calculator Quiz"
+                        onClick={() => navigate('/calculator')}
+                        variant="accent"
+                        style={{ fontSize: '24px', width: '600px', fontWeight: '200' }}
+                    />
+                </div>
+            )}
+        </div>
     );
 };
 
