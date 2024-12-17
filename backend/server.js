d79ab702547908094de13590f903876de0e90434
Shams Ahson <shams@Shamss-MacBook-Pro.local>
2024-12-17 00:07:10 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const getResources = require('./routes/getResources')
 
-app.use(cors());
+app.use(cors({
+  origin: allowedOrigins, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 app.use(bodyParser.json()); //NEW
+
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => console.log("Connected to MongoDB Atlas"))
